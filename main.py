942f66fd86ed7be6a9a327de13039c525ab2a444
salehsedghpour <salehsedghpour@mon.hcsone.ir>
2020-06-23 16:21:57 +0200

    test

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ with open('/opt/MinerControl/worker/workers.json') as json_file:
         zone = config['default']['zone']
         if worker['farm'] == farm and worker['zone'] == zone:
             date = getDate(worker['ip'],worker['port'],[worker['commands']['monitoring']])
+            if date['STATS'][1]['GHS 5s']:
+                hashrate = 0
+            else:
+                hashrate = 100
 
-            hashrate = date['STATS'][1]['GHS 5s']
+            #hashrate = date['STATS'][1]['GHS 5s']
             temprature = date['STATS'][1]['temp6']
             #check_network(worker['ip'], worker['rig'], worker['name'])
             check_hashrate(int(float(hashrate)), worker['rig'], worker['name'])
