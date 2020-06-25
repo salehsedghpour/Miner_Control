2069c2fb445f066942b5587b2000fcb8e0473a93
salehsedghpour <salehsedghpour@mon.hcsone.ir>
2020-06-25 18:02:35 +0200

    Version 1.0.0

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ from prometheus_client.exposition import basic_auth_handler
 
 
 def getDate(worker_ip, worker_port, command):
-    try:
+    #try:
         def linesplit(socket):
             buffer = socket.recv(4096).decode()
             done = False
@@ -32,8 +32,8 @@ def getDate(worker_ip, worker_port, command):
         response = json.loads(response)
         s.close()
         return response
-    except:
-        return False
+    #except:
+        #return False
 
 
 def getWorkers():
