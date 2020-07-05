2b693ea5b754cad0a36e9e6796bb61356c1e0424
salehsedghpour <salehsedghpour@mon.hcsone.ir>
2020-07-05 21:02:55 +0200

    add m3

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 (cd /opt/MinerControl/app ;git reset --hard;git remote add origin "https://github.com/salehsedghpour/miner_control";git pull origin master)
 username=$( awk -F "=" '/username/ {print $2}' /opt/MinerControl/config.ini)
 secret=$( awk -F "=" '/secret/ {print $2}' /opt/MinerControl/config.ini)
-(cd /opt/MinerControl/worker ;git reset --hard;git remote add origin "http://$username:$secret@mon.hcsone.ir:8929/$username/workers.git";git config --global user.email "$username@mon.hcsone.ir";git config --global user.name "$username";git config credential.helper store --file=/opt/MinerControl/.git-credentials)
+(cd /opt/MinerControl/worker ;git reset --hard;git remote add origin "http://$username:$secret@mon.hcsone.ir:8929/$username/workers.git";git config --global user.email "$username@mon.hcsone.ir";git config --global user.name "$username";git pull origin master)
